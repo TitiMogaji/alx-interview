e86a8a7aacf4b3e4de66b38e13a33221ee34f423
Titilope Mogaji <104643681+TitiMogaji@users.noreply.github.com>
2023-08-01 15:20:31 +0100

    update 0x01-lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,35 +1,17 @@
-#!/usr/bin/python3 
- """ 
- This is a module that provides a function for determining if all 
- boxes in a given list can be opened. 
- """ 
-  
-  
- def canUnlockAll(boxes): 
-     """ 
-     Check if all boxes can be opened
-    Args:
-        boxes (list): List which contain all the boxes with the keys
-    Returns:
-        bool: True if all boxes can be opened, otherwise, False
-    
-     """ 
-     n = len(boxes) 
-     seen_boxes = set([0]) 
-     unseen_boxes = set(boxes[0]).difference(set([0])) 
-     while len(unseen_boxes) > 0: 
-         boxIdx = unseen_boxes.pop() 
-         if not boxIdx or boxIdx >= n or boxIdx < 0: 
-             continue 
-         if boxIdx not in seen_boxes: 
-             unseen_boxes = unseen_boxes.union(boxes[boxIdx]) 
-             seen_boxes.add(boxIdx) 
-     return n == len(seen_boxes
-
-def main():
-    """Entry point"""
-    canUnlockAll([[]])
+#!/usr/bin/python3
+"""
+Module for canUnlockAll function"""
 
 
-if __name__ == '__main__':
-    main()
+def canUnlockAll(boxes):
+    """
+    Determines if all the boxes can be opened.
+    """
+    if boxes is None or len(boxes) == 0:
+        return False
+    keys = [0]
+    for key in keys:
+        for new_key in boxes[key]:
+            if new_key not in keys and new_key < len(boxes):
+                keys.append(new_key)
+    return len(keys) == len(boxes)
